28a1eb15f26a38e4f9662298d05529a1df9145a2
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-02-25 19:53:41 +0800

    data for frontEnd

--- backend/server.js
+++ backend/server.js
@@ -2,61 +2,106 @@ import { createServer } from "http";
 import { parse } from "url";
 import { WebSocketServer } from "ws";
 
+//configurables
+var onceTiming=["1200"];
+var twiceTiming=["1200","1400"];
+var thriceTiming=["0800","1200","1800"];
+
 // Create the https server
 const server = createServer();
 // Create two instance of the websocket server
-
 //ws1: FrontEnd to WebSocketServer
 //ws2: ESP32 to WebSocketServer
 const wss1 = new WebSocketServer({ noServer: true });
 const wss2 = new WebSocketServer({ noServer: true });
 
+//function to print stuff, send back to front end, logic to do backend
 // Take note of client or users connected
 const users = new Set();
 
+class pillState{
+  constructor(name){
+    this.name=name;
+    this.tracking=false;
+    this.count=0;
+    this.freq=1;
+    this.dispenseQty=2;
+    this.meal=false;
+    this.history={}; //{hour:qty taken}-- just 1 month
+  }
+  track(count,freq,dispenseQty,meal){
+    this.tracking=true;
+    this.count=count;
+    this.freq=freq;
+    this.dispenseQty=dispenseQty;
+    this.meal=meal;
+  }
+  refill(qty){
+    this.count+=qty;
+  }
+  dispense(){
+    this.count-=this.dispenseQty;
+    var current = new Date;
+    var month= current.getMonth();
+    var hour= current.getDate();
+    var key= month+','+hour;
+    //console.log(current.getHours());
+    if (key in this.history){
+      this.history[key]+=1;
+    }else{
+      this.history[key]=1;
+    }
+  }
+}
+var dataState=[]; //4 x pillStates
+dataState.push(new pillState("Pill_A"));
+dataState.push(new pillState("Pill_B"));
+dataState.push(new pillState("Pill_C"));
+dataState.push(new pillState("Liquid"));
 
-/*For the first connection "/request" path
- We take note of the clients that initiated connection and saved it in our list
- */
 wss1.on("connection", function connection(socket) {
   console.log("wss1:: User connected");
   const userRef = {
     socket: socket,
     connectionDate: Date.now(),
   };
-  console.log("Adding to set");
   users.add(userRef);
+
+  socket.on('message', function message (data){
+    const parseData = JSON.parse(data);
+    if (parseData==="import"){
+      sendData(); //send import to get data
+    }else{ //data sent is to update State
+      switch (data.name){
+        case "A":
+          dataState[0].track(data.count, data.freq, data.dispenseQty, data.meal);
+          break;
+        case "B":
+          dataState[1].track(data.count, data.freq, data.dispenseQty, data.meal);
+          break;
+        case "C":
+          dataState[2].track(data.count, data.freq, data.dispenseQty, data.meal);
+          break;
+        case "L":
+          dataState[3].track(data.count, data.freq, data.dispenseQty, data.meal);
+          break;  
+      }
+    }
+  })
 });
 
-/*
- For the second connection "/sendSensorData" path
- This is where we received the sensor reads from the ESP32 Dev module.
- Upon receiving the sensor read, we broadcast it to all the client listener
-*/
+
 wss2.on("connection", function connection(ws) {
   console.log("wss2:: socket connection ");
   //ESP32.add(ws);
   informESP32(ws,"text");
   ws.on('message', function message(data) {
       const now = Date.now();
-      //const parseData = JSON.parse(data);
-      //var data= parseData.split();
-      
-      //let taken={date: now}
-      //sendMessage(jsonMessage);
-      //save history into database?
-     console.log(now);
+     console.log(now.getDay());
      //informESP32(ws,"text");
   });
-
 });
 
-
-/*
-This is the part where we create the two paths.  
-Initial connection is on HTTP but is upgraded to websockets
-The two path "/request" and "/sendSensorData" is defined here
-*/
 server.on("upgrade", function upgrade(request, socket, head) {
   const { pathname } = parse(request.url);
   console.log(`Path name ${pathname}`);
@@ -73,14 +118,16 @@ server.on("upgrade", function upgrade(request, socket, head) {
     socket.destroy();
   }
 });
+
 //Open the server port in 8080
 server.listen(8080);
 
 //function to send websocket messages to user
-const sendMessage = (message) => {
-  // console.log("Sending messages to users!");
+const sendData = () => {
+  var data= JSON.stringify(dataState)
+  console.log(data);
   for (const user of users) {
-    user.socket.send(message);
+    user.socket.send(data);
   }
 };
 
@@ -88,4 +135,8 @@ function informESP32(ws, message){
   var dataStr="0,1,3,0,15,0"
   ws.send(JSON.stringify(dataStr));
   return;
+}
+
+function printInfo(){
+  console.log(dataState);
 }
